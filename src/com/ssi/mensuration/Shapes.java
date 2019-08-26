3317671364578d627cece0efc74df8e1a122319e
RahulR <intexjazz@gmail.com>
2019-08-27 05:23:24 +0530

    task 0 completed

---
+++ src/com/ssi/mensuration/Shapes.java
@@ -0,0 +1,13 @@
+package com.ssi.mensuration;
+
+import com.ssi.simple.Circle;
+
+public class Shapes {
+
+	public static void main(String[] args) {
+		Circle objCircle = new Circle();
+		System.out.println(objCircle.area(10));
+		
+	}
+
+}
